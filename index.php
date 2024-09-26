ea5f30dccc1046bd28211aaae79de58707502185
fatisajid <fatimaezahraa.salah@gmail.com>
2024-09-26 09:59:14 +0200

    cours POO

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//Création d'un instance de mon objet;
+$date = new DateTime();
+
+//Voir dans mon objet
+var_dump($date);
+
+//Voir le timezone de mon objet
+var_dump($date->getTimezone());
+
+//Modifier ma date et ajouter un jour
+$date->modify('+1 day');
+//Revoir dans mon objet
+var_dump($date);
+
+//Modifier la date et le format
+$formatDate = $date->modify('+ 1 day')->format('d/m/Y');
+echo '<pre>';
+var_dump($formatDate);
+echo '</pre>';
